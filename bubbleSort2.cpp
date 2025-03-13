24b412a53f5d2da928f0fe0734524af546cbc677
tari2025 <denilestari272@gmail.com>
2025-03-13 10:46:10 +0700

    add library dan deklarasi variabel global

---
+++ bubbleSort2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variable global array dengan ukuran 20
+int n; // Deklarasi variable global n untuk menyimpan banyaknya element pada array
