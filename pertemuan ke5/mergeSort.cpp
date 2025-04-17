bc15fb362ed3bfbda10e7a380594901c7698b7f2
tari2025 <denilestari272@gmail.com>
2025-04-17 10:47:24 +0700

    step

--- pertemuan ke5/mergeSort.cpp
+++ pertemuan ke5/mergeSort.cpp
@@ -35,3 +35,21 @@ void input()
             return; // step 1.a
         }
         int mid(low + high) / 2; // step 2
+        // step 3
+        // fungsi rekursi - memanggil diri sendiri
+        mergeSort(low, mid);      // step 3.a
+        mergeSort(mid + 1, high); // step 3.b
+
+        // step 4
+        int i = low          // step 4.a
+            int j = mid + 1; // step 4.b
+        int k = llow;        // step 4.c
+
+        while (i <= mid && j <= high)
+        { // step 4.d
+
+if (arr[i] <= arr[j] { // step 4.e
+    B[k] = arr[j; // step 4.e.i
+    j++;
+}
+while (j <= high) { } // step
